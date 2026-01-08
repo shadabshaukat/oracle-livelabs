f60f7f8d5f75b728ae5a85bf9d99914a00237001
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2026-01-08 18:19:43 +1100

    Add compute

--- oci_postgres_tf_stack/vars.tf
+++ oci_postgres_tf_stack/vars.tf
@@ -1,75 +1,135 @@
+variable "region" {
+  type    = string
+  default = "us-ashburn-1"
+}
 
-variable region { default = "us-ashburn-1" }
-#variable "tenancy_name" { }
-variable  compartment_ocid {   }
+variable "compartment_ocid" {
+  type = string
+}
 
+## Network
 
-## Network 
+variable "create_service_gateway" {
+  type    = bool
+  default = true
+}
 
-variable create_service_gateway { default = true }
-variable create_vcn_subnet { default = true }
-variable psql_subnet_ocid {  default = "" } ## Private Subnet OCID of existing Subnet 
-variable "vcn_cidr" { 
-    type = list
-    default = ["10.10.0.0/16"] 
-    }
+variable "create_vcn_subnet" {
+  type    = bool
+  default = true
+}
 
-variable "create_vault" { default = true }
-variable "vault_id" { 
+variable "psql_subnet_ocid" {
+  type        = string
+  description = "Private Subnet OCID of existing subnet (used when create_vcn_subnet = false)"
+  default     = ""
+}
+
+variable "vcn_cidr" {
+  type    = list(string)
+  default = ["10.10.0.0/16"]
+}
+
+## Vault/KMS
+
+variable "create_vault" {
+  type    = bool
+  default = true
+}
+
+variable "vault_id" {
+  type    = string
   default = ""
 }
-## PSQL 
+
+## PostgreSQL
+
 variable "psql_admin" {
-    type = string
-    description = "Name of PSQL Admin Username"
+  type        = string
+  description = "Name of PostgreSQL admin user"
 }
 
-variable psql_version {  default = 16 } 
-variable inst_count  { default = 1 }
+variable "psql_version" {
+  type    = number
+  default = 14
+}
 
-variable "num_ocpu" { default = 2 }
-  
+variable "inst_count" {
+  type    = number
+  default = 1
+}
+
+variable "num_ocpu" {
+  type    = number
+  default = 2
+}
+
 variable "psql_shape_name" {
+  type        = string
+  description = "PostgreSQL shape family name"
+  default     = "PostgreSQL.VM.Standard.E4.Flex"
+}
+
+variable "psql_iops" {
+  type = map(number)
+  default = {
+    75  = 75000
+    150 = 150000
+    225 = 225000
+    300 = 300000
+  }
+}
+
+# variable "psql_passwd_type" { default = "PLAIN_TEXT" }
+
+## Compute (optional)
+
+variable "create_compute" {
+  type    = bool
+  default = false
+}
+
+variable "compute_shape" {
   type    = string
-  default = "PostgreSQL.VM.Standard.E4.Flex"
+  default = "VM.Standard.E4.Flex"
 }
 
-variable psql_iops {
-   type = map(number)
-    default = {75000}
+variable "compute_ocpus" {
+  type    = number
+  default = 1
 }
 
-#variable psql_passwd_type  {  default = "PLAIN_TEXT" }
+variable "compute_memory_in_gbs" {
+  type    = number
+  default = 6
+}
 
-## Compute
+variable "compute_assign_public_ip" {
+  type    = bool
+  default = false
+}
 
-variable create_compute { default = false }
-
-variable "compute_shape" { default = "VM.Standard.E4.Flex" }
-
-variable "compute_ocpus" { default = 1 }
-
-variable "compute_memory_in_gbs" { default = 8 }
-
-variable "compute_assign_public_ip" { default = false }
-
-variable "compute_display_name" { default = "app-host-1" }
+variable "compute_display_name" {
+  type    = string
+  default = "app-host-1"
+}
 
 variable "compute_ssh_public_key" {
-  type = string
+  type    = string
   default = ""
 }
 
 variable "compute_image_ocid" {
-  type = string
+  type    = string
   default = ""
 }
 
 variable "compute_nsg_ids" {
-  type = list(string)
+  type    = list(string)
   default = []
 }
 
 variable "compute_boot_volume_size_in_gbs" {
+  type    = number
   default = 50
 }
